f593fb8b80d2e192eca397671d9c5dae655ff3d5
Stephen Cefali <scefali@sentry.io>
2023-10-08 12:27:08 -0700

    messing with docker

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,16 @@ FROM nvidia/cuda:12.1.0-base-ubuntu20.04
 RUN apt-get update && apt-get install -y software-properties-common
 RUN apt-get install -y g++-11 make python3 python-is-python3 pip
 
+# Install necessary dependencies and Python 3.10
+RUN apt-get update \
+    && apt-get install -y software-properties-common \
+    && add-apt-repository ppa:deadsnakes/ppa \
+    && apt-get update \
+    && apt-get install -y python3.10 python3.10-distutils python3.10-venv python3.10-dev python3-pip
+
+# Optionally set Python 3.10 as the default for "python3"
+RUN update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+
 # Copy just the requirements and install them first for caching
 COPY ./requirements.txt .
 RUN --mount=type=cache,target=/root/.cache CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install --timeout 100 -r requirements.txt
--- deploy_to_sagemaker.py
+++ deploy_to_sagemaker.py
@@ -30,30 +30,30 @@ role_arn = f"arn:aws:iam::{ACCOUNT_ID}:role/SageMaker"
 
 
 def deploy_model():
-    # device_type = "cuda" if torch.cuda.is_available() else "cpu"
-    # device_type = "cuda"
+    device_type = "cuda" if torch.cuda.is_available() else "cpu"
+    device_type = "cuda"
 
-    # # 1. Download the model
-    # # model = load_model(device_type, model_id=DEPLOYED_MODEL_ID, model_basename=DEPLOYED_MODEL_BASENAME)
-    # model, tokenizer = load_quantized_model_qptq(DEPLOYED_MODEL_ID, DEPLOYED_MODEL_BASENAME, device_type, logging)
+    # 1. Download the model
+    # model = load_model(device_type, model_id=DEPLOYED_MODEL_ID, model_basename=DEPLOYED_MODEL_BASENAME)
+    model, tokenizer = load_quantized_model_qptq(DEPLOYED_MODEL_ID, DEPLOYED_MODEL_BASENAME, device_type, logging)
 
-    # # 2. Save the model locally
-    # model_dir_local = "deployed_models"
+    # 2. Save the model locally
+    model_dir_local = "deployed_models"
 
-    # # Ensure local directory exists
-    # if not os.path.exists(model_dir_local):
-    #     os.makedirs(model_dir_local)
+    # Ensure local directory exists
+    if not os.path.exists(model_dir_local):
+        os.makedirs(model_dir_local)
 
-    # model.save_pretrained(model_dir_local)
-    # tokenizer.save_pretrained(model_dir_local)
+    model.save_pretrained(model_dir_local)
+    tokenizer.save_pretrained(model_dir_local)
 
-    # model_file = f"{model_dir_local}/model.tar.gz/model.bin"
-    # tokenizer_file = f"{model_dir_local}/tokenizer.tar.gz/tokenizer.model"
+    model_file = f"{model_dir_local}/model.tar.gz/model.bin"
+    tokenizer_file = f"{model_dir_local}/tokenizer.tar.gz/tokenizer.model"
 
-    # # 3. Upload to S3
-    # s3_client = boto3.client('s3')
-    # s3_client.upload_file(model_file, "legal-llama-model", os.path.basename(model_file))
-    # s3_client.upload_file(tokenizer_file, "legal-llama-model", os.path.basename(tokenizer_file))
+    # 3. Upload to S3
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(model_file, "legal-llama-model", os.path.basename(model_file))
+    s3_client.upload_file(tokenizer_file, "legal-llama-model", os.path.basename(tokenizer_file))
 
     # 4. Create the session
     sagemaker_session = Session()
